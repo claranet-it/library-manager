4c1d6f655c89d6e1a70f18fa90827040505cc30b
Simone Richini <simone.richini@it.clara.net>
2022-12-20 16:54:51 +0100

    feature:add entity book

---
+++ backend/src/Entity/Book/Book.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity\Book;
+
+use App\Repository\Book\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: Book::class)]
+class Book
+{
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\Column(length: 255)]
+    private string $name;
+
+    #[ORM\Column(length: 255)]
+    private string $author;
+
+    #[ORM\Column]
+    private float $price;
+
+    #[ORM\Column(length: 2000)]
+    private string $description;
+
+    public function __construct(int $id, string $name, string $author, float $price, string $description)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->author = $author;
+        $this->price = $price;
+        $this->description = $description;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthor(): string
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param string $author
+     */
+    public function setAuthor(string $author): void
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     */
+    public function setPrice(float $price): void
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+}
