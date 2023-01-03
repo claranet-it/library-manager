de951fdae43c775632e887401b713de4292f11d0
Paolo Rabbito <47228598+paolorabbito@users.noreply.github.com>
2023-01-03 12:32:29 +0100

    Feature/crud operations (#10)
    * (doc) add readme file for library api
    
    * (doc) add error codes to readme file
    
    * feature:add entity book
    
    * feature:migrations
    
    * feature:add book entity repository and controller
    
    * feature:add make command && pagination parameter
    
    * feature:add getBookById
    
    * refactoring
    
    * feature:add storing book
    
    * feature:fix storing book and refactoring pagination
    
    * refactoring data structure
    
    * feature:add DeleteBookController
    
    * feature:  add update book route and controller functionality
    
    * fix UpdateBookController
    
    * feature:add JsonSchemaValidator
    
    * feature:add xdebug && minorfix
    
    * feature:fix JsonSchemaValidator
    
    * feature:add nelmiocors bundle
    
    Co-authored-by: fesiddi <53375680+fesiddi@users.noreply.github.com>
    Co-authored-by: Simone Richini <simone.richini@it.clara.net>

--- backend/src/Book/Infrastructure/Controller/UpdateBookController.php
+++ backend/src/Book/Infrastructure/Controller/UpdateBookController.php
@@ -2,6 +2,8 @@
 
 namespace App\Book\Infrastructure\Controller;
 
+
+use App\Book\Infrastructure\JsonSchemaValidator;
 use App\Book\Infrastructure\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Exception\BadRequestException;
@@ -11,15 +13,22 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class UpdateBookController extends AbstractController
 {
-    public function __construct(private readonly BookRepository $bookRepository)
-    {
 
+    public function __construct(
+        private readonly BookRepository $bookRepository,
+        private readonly JsonSchemaValidator $jsonSchemaValidator
+    )
+    {
     }
 
     public function __invoke(Request $request): JsonResponse
     {
         if ($request->getContentTypeFormat() !== 'json')
             throw new BadRequestException('Invalid request format', 400);
+
+        $isValid = $this->jsonSchemaValidator->validate($request->getContent(), $this->requestJsonSchema());
+        if(!$isValid)
+            throw new BadRequestException('Invalid body format', 400);
 
         $body = json_decode($request->getContent(), true);
 
@@ -40,5 +49,27 @@ class UpdateBookController extends AbstractController
         return new JsonResponse([
             'book updated'
         ], 200);
+    }
+
+    protected function requestJsonSchema(): array
+    {
+        return [
+            'type' => 'object',
+            'required' => ['title', 'author', 'price'],
+            'properties' => [
+                'title' => [
+                    'type' => 'string',
+                ],
+                'author' => [
+                    'type' => 'string',
+                ],
+                'price' => [
+                    'type' => 'number',
+                ],
+                'description' => [
+                    'type' => 'string'
+                ]
+            ],
+        ];
     }
 }
--- backend/src/Book/Infrastructure/JsonSchemaValidator.php
+++ backend/src/Book/Infrastructure/JsonSchemaValidator.php
@@ -6,22 +6,15 @@ use JsonSchema\Validator;
 
 class JsonSchemaValidator
 {
-    public function validate($body, array $schema): array
+
+    public function validate($body, array $schema): bool
     {
         $validator = new Validator();
+        $data = json_decode($body);
 
-        $validator->validate($body, $schema);
+        $validator->validate($data, $schema);
 
-        return $validator->isValid() ? [] : self::formatErrors($validator);
+        return $validator->isValid();
     }
 
-    private static function formatErrors(Validator $validator): array
-    {
-        return array_map(
-            function (array $error): array {
-                return array_intersect_key($error, array_flip(['property', 'message']));
-            },
-            $validator->getErrors()
-        );
-    }
 }
