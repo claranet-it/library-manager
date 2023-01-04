bc7cb6d605cb85df4b2a187ea6bf03f24f44b9ac
mariateresagraziano <maria.teresa.graziano@it.clara.net>
2023-01-04 14:09:43 +0100

    ui: refactor code

--- frontend/src/components/home/bookList.tsx
+++ frontend/src/components/home/bookList.tsx
@@ -1,43 +1,41 @@
 import React, { useEffect, useState } from "react";
 import ReactPaginate from "react-paginate";
 import { Book } from "../../interface";
-import Spinner from "../Spinner";
+import Spinner from "../spinner";
 import BookCard from "./bookCard";
 
-function BookList(props: any) {
+function BookList() {
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
     const [books, setBooks] = useState([]);
     const [pageCount, setpageCount] = useState(0);
-    let limit = 5;
+    const limit = 5;
 
-    useEffect(() => {
-        const getBooks = async () => {
-            try {
-                setIsError(false);
-                setIsLoading(true);
+    const fetchBooksFirstCall = async () => {
+        try {
+            setIsError(false);
+            setIsLoading(true);
 
-                const requestOptions = {
-                    method: 'GET',
-                    headers: {
-                        'contentType': 'application/json'
-                    },
-                };
-                const res = await fetch(
-                    `http://localhost:8080/api/books?offset=0&limit=${limit}`, requestOptions
-                );
-                const data = await res.json();
-                const total: number = data.total;
-                setpageCount(Math.ceil(total / limit));
-                setBooks(data.data);
-
-            } catch (error) {
-                setIsError(true);
+            const requestOptions = {
+                method: 'GET',
+                headers: {
+                    'contentType': 'application/json'
+                },
             };
-            setIsLoading(false);
+            const res = await fetch(
+                `http://localhost:8080/api/books?offset=0&limit=${limit}`, requestOptions
+            );
+
+            const data = await res.json();
+            const total: number = data.total;
+            setpageCount(Math.ceil(total / limit));
+            setBooks(data.data);
+
+        } catch (error) {
+            setIsError(true);
         };
-        getBooks();
-    }, [limit]);
+        setIsLoading(false);
+    };
 
     const fetchBooks = async (offset: number) => {
         try {
@@ -59,6 +57,10 @@ function BookList(props: any) {
         };
     };
 
+    useEffect(() => {
+        fetchBooksFirstCall();
+    }, [limit]);
+
     const handleClicked = async (data: any) => {
         let offset = data.selected * limit;
         await fetchBooks(offset);
@@ -66,11 +68,9 @@ function BookList(props: any) {
         window.scrollTo(0, 0)
     };
 
-
-
     if (isLoading) return <Spinner />;
-    else if (isError) return <div className="info">Errore caricamento dati</div>;
-    else if (books.length != 0) {
+    if (isError) return <div className="info">Errore caricamento dati</div>;
+    if (books.length != 0) {
         return (
             <React.Fragment>
                 <div className="booklist">
@@ -79,31 +79,33 @@ function BookList(props: any) {
                     ))}
                 </div>
 
-                <ReactPaginate
-                    previousLabel={"<<"}
-                    nextLabel={">>"}
-                    breakLabel={"..."}
-                    pageCount={pageCount}
-                    marginPagesDisplayed={2}
-                    pageRangeDisplayed={3}
-                    onPageChange={handleClicked}
-                    containerClassName={"pagination"}
-                    pageClassName={"pagination__item"}
-                    pageLinkClassName={"pagination__link"}
-                    previousClassName={"pagination__controlls"}
-                    previousLinkClassName={"pagination__controlls"}
-                    nextClassName={"pagination__controlls"}
-                    nextLinkClassName={"pagination__controlls"}
-                    breakClassName={"pagination__item"}
-                    breakLinkClassName={"pagination__link"}
-                    activeClassName={"pagination__item--active"}
-                    activeLinkClassName={"pagination__link--active"} />
+                {pageCount > 1 &&
+
+                    <ReactPaginate
+                        previousLabel={"<<"}
+                        nextLabel={">>"}
+                        breakLabel={"..."}
+                        pageCount={pageCount}
+                        marginPagesDisplayed={2}
+                        pageRangeDisplayed={3}
+                        onPageChange={handleClicked}
+                        containerClassName={"pagination"}
+                        pageClassName={"pagination__item"}
+                        pageLinkClassName={"pagination__link"}
+                        previousClassName={"pagination__controlls"}
+                        previousLinkClassName={"pagination__controlls"}
+                        nextClassName={"pagination__controlls"}
+                        nextLinkClassName={"pagination__controlls"}
+                        breakClassName={"pagination__item"}
+                        breakLinkClassName={"pagination__link"}
+                        activeClassName={"pagination__item--active"}
+                        activeLinkClassName={"pagination__link--active"} />
+                }
             </React.Fragment>
         );
     }
-    else {
-        return <div className="info">Non ci sono libri nel catalogo</div >
-    }
+    return <div className="info">Non ci sono libri nel catalogo</div >
+
 };
 
 export default BookList;
--- frontend/src/views/home/index.tsx
+++ frontend/src/views/home/index.tsx
@@ -1,74 +1,7 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import BookList from '../../components/home/bookList';
-import { Book } from '../../interface';
 
 function Home() {
-  const [books, setBooks] = useState<Book[]>([
-    {
-      id: 1,
-      title: 'Extreme Contracts: Il knowledge work dalla negoziazione alla collaborazione',
-      author: 'Jacopo Romei',
-      price: 9.99,
-    },
-    {
-      id: 2,
-      title: 'Extreme Programming Explained: Embrace Change',
-      author: 'Kent Beck',
-      price: 37.86,
-    },
-    {
-      id: 3,
-      title: 'User Stories Applied: For Agile Software Development',
-      author: 'Cohn Mike',
-      price: 41.07,
-    },
-    {
-      id: 4,
-      title:
-        "The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses",
-      author: 'Eric Ries',
-      price: 25.17,
-    },
-    {
-      id: 5,
-      title: "Start Small, Stay Small: A Developer's Guide to Launching a Startup",
-      author: 'Rob Walling',
-      price: 21.32,
-    },
-    {
-      id: 1,
-      title: 'Extreme Contracts: Il knowledge work dalla negoziazione alla collaborazione',
-      author: 'Jacopo Romei',
-      price: 9.99,
-    },
-    {
-      id: 2,
-      title: 'Extreme Programming Explained: Embrace Change',
-      author: 'Kent Beck',
-      price: 37.86,
-    },
-    {
-      id: 3,
-      title: 'User Stories Applied: For Agile Software Development',
-      author: 'Cohn Mike',
-      price: 41.07,
-    },
-    {
-      id: 4,
-      title:
-        "The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses",
-      author: 'Eric Ries',
-      price: 25.17,
-    },
-    {
-      id: 5,
-      title: "Start Small, Stay Small: A Developer's Guide to Launching a Startup",
-      author: 'Rob Walling',
-      price: 21.32,
-    },
-  ]);
-
   return (
     <div className="page home">
       <div className="topbar home__topbar">
@@ -79,7 +12,7 @@ function Home() {
           </Link>
         </div>
       </div>
-      <BookList books={books} />
+      <BookList />
     </div>
   );
 }
