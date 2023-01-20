4431db7e976258808d716e7587197c7a3d30741c
Simone Richini <simone.richini@it.clara.net>
2023-01-20 16:01:51 +0100

    tests:add test

--- backend/tests/Unit/StoreBookTest.php
+++ backend/tests/Unit/StoreBookTest.php
@@ -31,4 +31,30 @@ class StoreBookTest extends TestCase
         $storeBook->storeBook($price,$author,$title,$description);
     }
 
+    public function testItShouldStoreUpdatedBook(): void
+    {
+        $price = 10.99;
+        $author = 'Author';
+        $title = 'Title';
+        $description = 'This book talks about a description';
+
+        $book = new Book();
+        $book->setDescription('Test inserimento')
+            ->setTitle('Titolo di test')
+            ->setAuthor('Autore di test')
+            ->setPrice(20.99);
+
+        $bookRepository = $this->prophesize(BookRepository::class);
+
+        $bookRepository->save(Argument::type(Book::class),true)->shouldBeCalledOnce();
+
+        $storeBook = new StoreBook($bookRepository->reveal());
+        $newBook = $storeBook->storeBook($price,$author,$title,$description, $book);
+
+        self::assertEquals($title,$newBook->getTitle());
+        self::assertEquals($author,$newBook->getAuthor());
+        self::assertEquals($price,$newBook->getPrice());
+        self::assertEquals($description,$newBook->getDescription());
+    }
+
 }
