0dd09d029363ee346d0b96ed5c308edb1dbcdcc4
Simone Richini <simone.richini@it.clara.net>
2023-01-26 09:33:09 +0100

    refactor:extract dump file method

--- backend/src/Book/Infrastructure/BookImporter.php
+++ backend/src/Book/Infrastructure/BookImporter.php
@@ -58,18 +58,23 @@ class BookImporter
         }
         catch (\Throwable $e) {
             $this->em->getConnection()->commit();
-            echo("Errore trovato sulla riga {$i}+1 del csv\n");
-            echo("Le prime {$i} righe del file csv sono state caricate correttamente\n");
-            echo("Correggere la prima riga del file csv e rilanciare lo script\n");
-            fputcsv($tempFile, $headers, ';');
-            $remainingRows = array_slice($data,$i);
-            foreach($remainingRows as $row){
-                fputcsv($tempFile, $row, ';');
-            }
-            fclose($tempFile);
-            unlink(\dirname(__DIR__) . "/../../public/{$fileName}");
-            rename(\dirname(__DIR__) . "/../../public/temp_{$fileName}", \dirname(__DIR__) . "/../../public/{$fileName}");
+            $this->dumpRestOfTheFile($i, $tempFile, $headers, $data, $fileName);
         }
+    }
+
+    private function dumpRestOfTheFile(int $i, bool $tempFile, bool|array $headers, mixed $data, string $fileName): void
+    {
+        echo("Errore trovato sulla riga {$i}+1 del csv\n");
+        echo("Le prime {$i} righe del file csv sono state caricate correttamente\n");
+        echo("Correggere la prima riga del file csv e rilanciare lo script\n");
+        fputcsv($tempFile, $headers, ';');
+        $remainingRows = array_slice($data, $i);
+        foreach ($remainingRows as $row) {
+            fputcsv($tempFile, $row, ';');
+        }
+        fclose($tempFile);
+        unlink(\dirname(__DIR__) . "/../../public/{$fileName}");
+        rename(\dirname(__DIR__) . "/../../public/temp_{$fileName}", \dirname(__DIR__) . "/../../public/{$fileName}");
     }
 
 }
