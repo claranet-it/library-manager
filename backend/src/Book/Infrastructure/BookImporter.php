6ebcd18e5f698bebee4e40cd90568dd45967badf
fesiddi <53375680+fesiddi@users.noreply.github.com>
2023-02-01 23:56:50 +0100

    refactor file handling functionality

--- backend/src/Book/Infrastructure/BookImporter.php
+++ backend/src/Book/Infrastructure/BookImporter.php
@@ -3,26 +3,23 @@
 namespace App\Book\Infrastructure;
 
 use App\Book\Application\StoreBook;
-use http\Exception\InvalidArgumentException;
+use Symfony\Component\Serializer\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Serializer\Encoder\CsvEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 
 class BookImporter
 {
     public function __construct(
         private readonly JsonSchemaValidator $jsonSchemaValidator,
-        private readonly StoreBook $storeBook
+        private readonly StoreBook $storeBook,
+        private readonly FileHandler $fileHandler
     ) {
     }
 
     public function import(OutputInterface $output, string $fileName): void
     {
-        $serializer = new Serializer([new ObjectNormalizer()], [new CsvEncoder()]);
         $file = fopen(\dirname(__DIR__)."/../../public/{$fileName}", 'r');
 
-        [$headers, $data, $dataLen] = $this->csvReader($file, $fileName, $serializer);
+        [$headers, $data, $dataLen] = $this->fileHandler->csvToArray($file, $fileName);
 
         $tempFile = fopen(\dirname(__DIR__)."/../../public/temp_{$fileName}", 'w');
         try {
@@ -30,57 +27,11 @@ class BookImporter
                 $data = $this->storeRowData($data, $i);
             }
             fclose($tempFile);
-            $this->filesCleaner($fileName);
+            $this->fileHandler->cleanFiles($fileName);
             echo "\033[32m File csv caricato completamente\n \033[0m";
         } catch (InvalidArgumentException $e) {
-            $this->dumpRestOfTheFile($i, $tempFile, $headers, $data, $fileName);
+            $this->fileHandler->dumpRestOfTheFile($i, $tempFile, $headers, $data, $fileName);
         }
-    }
-
-    /**
-     * @param mixed $file
-     * @param string $fileName
-     * @param Serializer $serializer
-     * @return mixed[]
-     */
-    private function csvReader(mixed $file, string $fileName, Serializer $serializer): array
-    {
-        if (!file_exists(\dirname(__DIR__)."/../../public/{$fileName}.copy")) {
-            copy(\dirname(__DIR__)."/../../public/{$fileName}", \dirname(__DIR__)."/../../public/{$fileName}.copy");
-        }
-
-        $headers = fgetcsv($file, 0, ';');
-        fclose($file);
-        $data = $serializer->decode(file_get_contents(\dirname(__DIR__)."/../../public/{$fileName}"), 'csv', [CsvEncoder::DELIMITER_KEY => ';']);
-        $dataLen = count($data);
-
-        return [$headers, $data, $dataLen];
-    }
-
-    /**
-     * @param int $i
-     * @param mixed $tempFile
-     * @param string[] $headers
-     * @param mixed $data
-     * @param string $fileName
-     * @return void
-     */
-    private function dumpRestOfTheFile(int $i, mixed $tempFile, array $headers, mixed $data, string $fileName): void
-    {
-        $lineOfError = $i + 1;
-        echo "\033[32m Errore trovato sulla riga {$lineOfError} del csv\n";
-        echo "Le prime {$i} righe del file csv sono state caricate correttamente\n";
-        echo "Correggere la prima riga del file csv e rilanciare lo script\n \033[0m";
-
-        fputcsv($tempFile, $headers, ';');
-
-        $remainingRows = array_slice($data, $i);
-        foreach ($remainingRows as $row) {
-            fputcsv($tempFile, $row, ';');
-        }
-        fclose($tempFile);
-        unlink(\dirname(__DIR__)."/../../public/{$fileName}");
-        rename(\dirname(__DIR__)."/../../public/temp_{$fileName}", \dirname(__DIR__)."/../../public/{$fileName}");
     }
 
     /**
@@ -105,13 +56,5 @@ class BookImporter
         }
 
         return $data;
-    }
-
-    public function filesCleaner(string $fileName): void
-    {
-        unlink(\dirname(__DIR__)."/../../public/{$fileName}");
-        rename(\dirname(__DIR__)."/../../public/temp_{$fileName}", \dirname(__DIR__)."/../../public/{$fileName}");
-        unlink(\dirname(__DIR__)."/../../public/{$fileName}");
-        rename(\dirname(__DIR__)."/../../public/{$fileName}.copy", \dirname(__DIR__)."/../../public/files/imported/{$fileName}");
     }
 }
---
+++ backend/src/Book/Infrastructure/FileHandler.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Book\Infrastructure;
+
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class FileHandler
+{
+    protected $serializer;
+
+    public function __construct() 
+        {
+            $this->serializer = new Serializer([new ObjectNormalizer()], [new CsvEncoder()]); 
+        }
+
+
+    /**
+     * @param mixed $file
+     * @param string $fileName
+     * @return mixed[]
+     */
+    public function csvToArray(mixed $file, string $fileName): array
+    {
+        if (!file_exists(\dirname(__DIR__)."/../../public/{$fileName}.copy")) {
+            copy(\dirname(__DIR__)."/../../public/{$fileName}", \dirname(__DIR__)."/../../public/{$fileName}.copy");
+        }
+
+        $headers = fgetcsv($file, 0, ';');
+        fclose($file);
+        $data = $this->serializer->decode(file_get_contents(\dirname(__DIR__)."/../../public/{$fileName}"), 'csv', [CsvEncoder::DELIMITER_KEY => ';']);
+        $dataLen = count($data);
+
+        return [$headers, $data, $dataLen];
+    }
+
+    /**
+     * @param int $i
+     * @param mixed $tempFile
+     * @param string[] $headers
+     * @param mixed $data
+     * @param string $fileName
+     * @return void
+     */
+    public function dumpRestOfTheFile(int $i, mixed $tempFile, array $headers, mixed $data, string $fileName): void
+    {
+        $lineOfError = $i + 1;
+        echo "\033[31m Errore trovato sulla riga {$lineOfError} del csv\n";
+        echo "\033[31m Le prime {$i} righe del file csv sono state caricate correttamente\n";
+        echo "\033[31m Correggere la prima riga del file csv e rilanciare lo script\n \033[0m";
+
+        fputcsv($tempFile, $headers, ';');
+
+        $remainingRows = array_slice($data, $i);
+        foreach ($remainingRows as $row) {
+            fputcsv($tempFile, $row, ';');
+        }
+        fclose($tempFile);
+        unlink(\dirname(__DIR__)."/../../public/{$fileName}");
+        rename(\dirname(__DIR__)."/../../public/temp_{$fileName}", \dirname(__DIR__)."/../../public/{$fileName}");
+    }
+
+
+    public function cleanFiles(string $fileName): void
+    {
+        unlink(\dirname(__DIR__)."/../../public/{$fileName}");
+        rename(\dirname(__DIR__)."/../../public/temp_{$fileName}", \dirname(__DIR__)."/../../public/{$fileName}");
+        unlink(\dirname(__DIR__)."/../../public/{$fileName}");
+        $destination = \dirname(__DIR__) . "/../../public/files/imported/";
+        if (!is_dir($destination)) {
+            mkdir($destination, 0777, true);
+        }
+        rename(\dirname(__DIR__) . "/../../public/{$fileName}.copy", $destination . "{$fileName}");        
+    }
+}
