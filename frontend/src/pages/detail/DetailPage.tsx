33664270b3c5fce8d47a2a9141ece0cd3d416abb
mariateresagraziano <maria.teresa.graziano@it.clara.net>
2023-03-14 16:10:40 +0100

    add edit modal

--- frontend/src/pages/detail/DetailPage.tsx
+++ frontend/src/pages/detail/DetailPage.tsx
@@ -4,9 +4,10 @@ import { BOOK } from '../../api/bookClient';
 import Arrow from '../../assets/icon/arrow-left-solid.svg';
 import { stockData } from '../../model/label';
 import { STATUS } from '../../model/status';
-import { Book, TError, ToastContextType } from '../../model/types';
+import { Book, ModalContextType, OmitID, TError, ToastContextType } from '../../model/types';
 import { ErrorMessage } from '../../shared/components/error';
 import { Spinner } from '../../shared/components/spinner/Spinner';
+import { ModalSetState } from '../../shared/context/modalContext';
 import { ToastSetState } from '../../shared/context/toastContext';
 import { BookDetail } from './components/BookDetail';
 
@@ -16,11 +17,31 @@ export const DetailPage: React.FC = (): React.ReactElement => {
   const [book, setBook] = useState<Book | null>(null);
   const [error, setError] = useState<TError>({ isError: false, message: '' });
   const { addToast } = useContext(ToastSetState) as ToastContextType;
+  const { closeModal } = useContext(ModalSetState) as ModalContextType;
 
   const { id = '' } = useParams();
 
-  const handleEdit = () => {
-    navigate(`/edit/${id}`, { replace: true });
+  const handleEdit = async (body: OmitID<Book>): Promise<void> => {
+    try {
+      await BOOK.update({
+        id,
+        ...body,
+      });
+      addToast({
+        type: STATUS.SUCCESS,
+        title: stockData.toastMessage.titleSuccess,
+        message: stockData.toastMessage.put,
+      });
+      getBook();
+      closeModal();
+    } catch (error: unknown) {
+      const message = error instanceof Error ? error.message : stockData.error;
+      addToast({
+        type: STATUS.ERROR,
+        title: stockData.toastMessage.titleError,
+        message,
+      });
+    }
   };
 
   const handleDelete = async (id: string) => {
--- frontend/src/pages/detail/components/BookDetail.tsx
+++ frontend/src/pages/detail/components/BookDetail.tsx
@@ -1,20 +1,21 @@
 import { useContext } from 'react';
 import Pen from '../../../assets/icon/pen-solid.svg';
 import Trash from '../../../assets/icon/trash-solid.svg';
-import { Book, ModalContextType } from '../../../model/types';
+import { Book, ModalContextType, OmitID } from '../../../model/types';
+import { BookForm } from '../../../shared/components';
 import { ModalSetState } from '../../../shared/context/modalContext';
 import { ModalDelete } from './ModalDelete';
 
 type Props = {
   book: Book;
   onDelete: () => void;
-  onEdit: () => void;
+  onEdit: (values: OmitID<Book>) => Promise<void>;
 };
 
 export const BookDetail: React.FC<Props> = ({ book, onDelete, onEdit }) => {
   const { openModal, closeModal, setChildren } = useContext(ModalSetState) as ModalContextType;
 
-  const handleModal = () => {
+  const handleModalDelete = () => {
     setChildren(
       <ModalDelete
         onConfirm={() => {
@@ -29,15 +30,28 @@ export const BookDetail: React.FC<Props> = ({ book, onDelete, onEdit }) => {
     openModal();
   };
 
+  const handleModalEdit = () => {
+    setChildren(
+      <BookForm
+        onSubmit={onEdit}
+        onCancel={() => {
+          closeModal();
+        }}
+        values={book}
+      />
+    );
+    openModal();
+  };
+
   return (
     <div className="book">
       <div className="book__poster"></div>
       <div className="book__detail">
         <div className="book__actions">
-          <button onClick={onEdit}>
+          <button onClick={handleModalEdit}>
             <img className="edit" src={Pen} alt="back" height="20px" /> Modifica
           </button>
-          <button onClick={handleModal}>
+          <button onClick={handleModalDelete}>
             <img className="delete" src={Trash} alt="back" height="20px" />
             Elimina
           </button>
