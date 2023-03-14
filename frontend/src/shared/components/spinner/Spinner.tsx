f3ff0679166082f946a28eef643934d19fde56eb
mariateresagraziano <maria.teresa.graziano@it.clara.net>
2023-03-14 12:57:40 +0100

    fix page spinner

--- frontend/src/shared/components/spinner/Spinner.tsx
+++ frontend/src/shared/components/spinner/Spinner.tsx
@@ -1,6 +1,6 @@
 export const Spinner: React.FC = (): React.ReactElement => {
   return (
-    <div className="center-spinner">
+    <div className="page center-spinner">
       <div className="lds-circle">
         <div></div>
       </div>
