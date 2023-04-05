b83bb885e78446391e04bd52fe3fbfbe9041132a
mariateresagraziano <maria.teresa.graziano@it.clara.net>
2023-04-05 14:40:03 +0200

    refactor modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,13 @@
 import './assets/css/app.scss';
 import AppRouter from './router/AppRouter';
-import { ModalProvider } from './shared/context/modalContext';
-import { ModalManager } from './shared/context/modalManager';
 import { ToastProvider } from './shared/context/toastContext';
 import { ToastManager } from './shared/context/toastManager';
 
 function App() {
   return (
     <ToastProvider>
-      <ModalProvider>
-        <AppRouter></AppRouter>
-        <ToastManager />
-        <ModalManager />
-      </ModalProvider>
+      <AppRouter></AppRouter>
+      <ToastManager />
     </ToastProvider>
   );
 }
--- frontend/src/model/modal.ts
+++
@@ -1,6 +0,0 @@
-export type ModalContextType = {
-  show: boolean;
-  content: React.ReactNode;
-  openModal: (content: React.ReactNode) => void;
-  closeModal: () => void;
-};
