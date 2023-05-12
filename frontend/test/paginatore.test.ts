70d3478525fd8cf8f3125191b5eb8982e891c47d
FrancescoSanti96 <francescosanti123@gmail.com>
2023-05-12 17:00:20 +0200

    modify test

--- frontend/test/paginatore.test.ts
+++ frontend/test/paginatore.test.ts
@@ -40,8 +40,56 @@ describe('paginatore', () => {
     expect(paginatore(2, 4)).toStrictEqual(['1', '2', '3']);
   });
 
-  test('should return three pages with extrema pages', () => {
-    expect(paginatore(1, 4)).toStrictEqual(['1', '2', '3']);
-    expect(paginatore(4, 4)).toStrictEqual(['2', '3', '4']);
+  // test('should return three pages with extrema pages', () => {
+  //   expect(paginatore(1, 4)).toStrictEqual(['1', '2', '3']);
+  //   expect(paginatore(4, 4)).toStrictEqual(['2', '4', '4']);
+  // });
+
+  describe('caso normale', () => {
+    test.each([
+      [1, 10, ['1', '2', '3']],
+      [2, 10, ['1', '2', '3']],
+      [3, 10, ['2', '3', '4']],
+      [4, 10, ['3', '4', '5']],
+      [5, 10, ['4', '5', '6']],
+      [6, 10, ['5', '6', '7']],
+      [7, 10, ['6', '7', '8']],
+      [8, 10, ['7', '8', '9']],
+      [9, 10, ['8', '9', '10']],
+      [10, 10, ['8', '9', '10']],
+    ])('paginatore(currentePage=%i, totalPages=%i) -> %s', (a, b, c) => {
+      expect(paginatore(a, b)).toStrictEqual(c);
+    });
+  });
+
+  describe('caso limite meno pagine che bottoni', () => {
+    test.each([
+      [1, 1, ['1']],
+      [1, 2, ['1', '2']],
+      [2, 2, ['1', '2']],
+      [1, 3, ['1', '2', '3']],
+      [2, 3, ['1', '2', '3']],
+      [3, 3, ['1', '2', '3']],
+    ])('paginatore(currentePage=%i, totalPages=%i) -> %s', (a, b, c) => {
+      expect(paginatore(a, b)).toStrictEqual(c);
+    });
+  });
+
+  describe('pagina non valida', () => {
+    test.each([
+      [0, 1, []],
+      [2, 1, []],
+      [11, 10, []],
+      [-4, 5, []],
+      [2, 0, []],
+      [0, 0, []],
+      [-1, 0, []],
+    ])('paginatore(currentePage=%i, totalPages=%i) -> %s', (a, b, c) => {
+      expect(paginatore(a, b)).toStrictEqual(c);
+    });
   });
 });
+
+// TODO
+// Aggiungere test-each matriciale
+// Una expectation per ogni test
